34115fb22c6fee1bdcfc4d7586f496ff87027916
ghost <ghost@ghosts-MacBook-Pro.local>
2022-07-30 14:57:08 -0600

    url is updated

--- pages/api/form_response.js
+++ pages/api/form_response.js
@@ -4,5 +4,5 @@ export default function handler(req, res) {
   // res.status(200).json({ data: req })
   // res.send( req )
   // res.send( req.body )
-  res.send( "<html> <head></head><body><h1>Hobby form</h1><p>nickname:" +req.body.nickname + "</p><p>email: " + req.body.email + "</p><p>phone number: " + req.body.telno + "</p><p>location:" + req.body.location + "</p><p><a href=\"http://localhost:3000\">Return to Hobby page</a></p>" + "</body></html>" )
+  res.send( "<html> <head></head><body><h1>Hobby form</h1><p>nickname:" +req.body.nickname + "</p><p>email: " + req.body.email + "</p><p>phone number: " + req.body.telno + "</p><p>location:" + req.body.location + "</p><p><a href=\"http://dev.saitnewmedia.ca/~gcheng/mmda222/hobby/\">Return to Hobby page</a></p>" + "</body></html>" )
 }
